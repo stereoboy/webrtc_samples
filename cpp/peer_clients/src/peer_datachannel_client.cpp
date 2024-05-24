e370f622bad370fad4b2ad198379ecca6286c277
Jaeyoung Lee <jylee0121@gmail.com>
2024-05-24 16:51:46 +0900

    update: attach socket.io-client-cpp to webRTC peer client
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- cpp/peer_clients/src/peer_datachannel_client.cpp
+++ cpp/peer_clients/src/peer_datachannel_client.cpp
@@ -1,3 +1,21 @@
+
+#include <sio_client.h>
+#include <asio.hpp>
+#include <asio/ssl.hpp>
+
+// #include <api/create_peerconnection_factory.h>
+#include <rtc_base/ssl_adapter.h>
+// #include <rtc_base/thread.h>
+// #include <system_wrappers/include/field_trial.h>
+
+
 int main(int argc, char* argv[]) {
+    asio::ssl::context *ssl_ctx = new asio::ssl::context(asio::ssl::context::tls);
+
+    sio::client sio_client();
+
+    rtc::InitializeSSL();
+
+    rtc::CleanupSSL();
     return 0;
 }
